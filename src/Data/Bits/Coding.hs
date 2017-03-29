be7eea3931ebc951fd140c4829153067b0c8e0ac
Luke Clifton <ltclifton@gmail.com>
2017-03-29 11:24:27 +0800

    Fix off-by-one error in putUnaligned

--- src/Data/Bits/Coding.hs
+++ src/Data/Bits/Coding.hs
@@ -206,7 +206,7 @@ putAligned m = Coding $ \ k i b ->
 
 -- | 'Put' all the bits without a 'flush'
 putUnaligned :: (MonadPut m, FiniteBits b) => b -> Coding m ()
-putUnaligned b = putBitsFrom (finiteBitSize b) b
+putUnaligned b = putBitsFrom (pred $ finiteBitSize b) b
 {-# INLINE putUnaligned #-}
 
 -- | 'Put' a single bit, emitting an entire 'byte' if the bit buffer is full
